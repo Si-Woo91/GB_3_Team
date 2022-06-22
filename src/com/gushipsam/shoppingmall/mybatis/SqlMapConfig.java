48d2fbc69b09bc592160623524636ba60873e21e
USER <USER@KimSiWoo>
2022-06-22 14:05:50 +0900

    22.06.22 14:05 (수정.......)(KSW)

--- src/com/gushipsam/shoppingmall/mybatis/SqlMapConfig.java
+++ src/com/gushipsam/shoppingmall/mybatis/SqlMapConfig.java
@@ -13,7 +13,7 @@ public class SqlMapConfig {
 	// 클래스 초기화 블럭(클래스가 처음 로딩될 때 한번만 수행)
 	static {
 		try {
-			String resource = ".com/koreait/shoppingmall/mybatis/config.xml";
+			String resource = ".com/gushipsam/shoppingmall/mybatis/config.xml";
 			Reader reader = Resources.getResourceAsReader(resource);
 			factory = new SqlSessionFactoryBuilder().build(reader);
 		} catch (IOException e) {
