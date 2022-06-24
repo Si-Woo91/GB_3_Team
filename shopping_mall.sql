da39a648a3338c7118a23d3d16dd7e85b156c449
smart7cw <104415352+smart7cw@users.noreply.github.com>
2022-06-24 21:45:06 +0900

    Update shopping_mall.sql
    CART 테이블 fk 조건 추가 완료
    ORDERS 테이블 주문수량컬럼 oQTY 추가 완료
    (찬우, 2022.06.24)

--- shopping_mall.sql
+++ shopping_mall.sql
@@ -7,73 +7,79 @@
 --DROP TABLE FK_CART_MEM ;
 --DROP TABLE FK_CART_PRODUCT ;
 
-CREATE TABLE ADMIN(						-- ����������
-	aID VARCHAR2(10) PRIMARY KEY,		-- ������ ���̵�
-	aPW VARCHAR2(100),					-- ������ ��й�ȣ
-	aNAME VARCHAR2(100)					-- ������ �̸�
+CREATE TABLE ADMIN(						-- 관리자정보
+	aID VARCHAR2(10) PRIMARY KEY,		-- 관리자 아이디
+	aPW VARCHAR2(100),					-- 관리자 비밀번호
+	aNAME VARCHAR2(100)					-- 관리자 이름
 );
 
-CREATE TABLE "MEMBER"(					-- ȸ������
-    mID VARCHAR2(100) PRIMARY KEY,		-- ȸ�� ���̵�
-    mPW VARCHAR2(15), 					-- ȸ�� ��й�ȣ
-    mNickNAME VARCHAR2(100),			-- ȸ�� �г���
-    mEmail VARCHAR2(50),				-- ȸ�� �̸��� 
-    mPHONE VARCHAR2(100)				-- ȸ�� ��ȭ��ȣ
+CREATE TABLE MEMBER(					-- 회원정보
+    mID VARCHAR2(100) PRIMARY KEY,		-- 회원 아이디
+    mPW VARCHAR2(15), 					-- 회원 비밀번호
+    mNickNAME VARCHAR2(100),			-- 회원 닉네임
+    mEmail VARCHAR2(50),				-- 회원 이메일 
+    mPHONE VARCHAR2(100)				-- 회원 전화번호
 );
 
-CREATE TABLE GOODS(					-- ��ǰ ����
-	gID NUMBER(4) PRIMARY KEY,			-- ��ǰ ���̵�
-    gNumber NUMBER(4),					-- ��ǰ ��ȣ
-    gName VARCHAR2(100),				-- ��ǰ �̸�
-    gBrand VARCHAR2(100),				-- ��ǰ �귣���
-    gPRICE NUMBER(10),					-- ��ǰ ����
-    gSTOCK NUMBER(3),					-- ��ǰ ����
-    gDATE DATE DEFAULT SYSDATE			-- ��ǰ �����
+CREATE TABLE GOODS(					-- 상품 정보
+	gID NUMBER(4) PRIMARY KEY,			-- 상품 아이디
+    gNumber NUMBER(4),					-- 상품 번호
+    gName VARCHAR2(100),				-- 상품 이름
+    gBrand VARCHAR2(100),				-- 상품 브랜드명
+    gPRICE NUMBER(10),					-- 상품 가격
+    gSTOCK NUMBER(3),					-- 상품 수량
+    gDATE DATE DEFAULT SYSDATE			-- 상품 등록일
 );
 
-CREATE TABLE CART(								-- ��ٱ���
+--CART 테이블 fk 조건 추가 완료(찬우, 2022.06.24)
+CREATE TABLE CART(								-- 장바구니
 	cID NUMBER(10) PRIMARY KEY,
     mID VARCHAR2(100),								
     gID NUMBER(4),						
-    cQTY NUMBER(3) DEFAULT 1					-- ��ٱ��Ͽ� ����ִ� ��ǰ�� ����
+    cQTY NUMBER(3) DEFAULT 1,					-- 장바구니에 담겨있는 상품의 수량
+CONSTRAINT fk_cart_mem FOREIGN KEY(mID) 
+REFERENCES member(mID),
+CONSTRAINT fk_cart_goods FOREIGN KEY(gID) 
+REFERENCES GOODS(gID)
 );
 
-CREATE TABLE fk_Cart_Mem(						-- ��ٱ���, ��� fk
-	 mID VARCHAR2(500)
-);
+--CREATE TABLE fk_Cart_Mem(						-- 장바구니, 멤버 fk
+--	 mID VARCHAR2(500)
+--);
 
-CREATE TABLE fk_Cart_Product(					-- ��ٱ���, ��ǰ fk
-	  gNumber NUMBER(4)
-);
+--CREATE TABLE fk_Cart_Product(					-- 장바구니, 상품 fk
+--	  gNumber NUMBER(4)
+--);
 
-
-CREATE TABLE ORDERS(							-- �ֹ�����
+--ORDERS 테이블 주문수량컬럼 oQTY 추가 완료(찬우, 2022.06.24)
+CREATE TABLE ORDERS(							-- 주문내역
     oID NUMBER(10) PRIMARY KEY,					
     mID VARCHAR2(100),	
-    gID NUMBER(4),								
-    oDATE DATE DEFAULT SYSDATE,					-- �ֹ� ��¥
-    oPHONE VARCHAR2(100),						-- �ֹ��� ����ó
-    oADDRESS VARCHAR2(500),						-- �ּ�				
-    oPAYMENT NUMBER(20),						-- �����ݾ�
---    oINVOICE NUMBER(10),						-- �����ȣ
+    gID NUMBER(4),
+    oQTY NUMBER(3) DEFAULT 1,					--주문 수량
+    oDATE DATE DEFAULT SYSDATE,					-- 주문 날짜
+    oPHONE VARCHAR2(100),						-- 주문자 연락처
+    oADDRESS VARCHAR2(500),						-- 주소				
+    oPAYMENT NUMBER(20),						-- 결제금액
+--    oINVOICE NUMBER(10),						-- 송장번호
 CONSTRAINT fk_ord_mem FOREIGN KEY(mID) 
 REFERENCES member(mID),
 CONSTRAINT fk_ord_goods FOREIGN KEY(gID) 
 REFERENCES GOODS(gID)
 );
 
-CREATE TABLE ImageFile(							-- �̹�������
+CREATE TABLE ImageFile(							-- 이미지관리
 	iID number(10) PRIMARY KEY,					
-	fileNm varchar2(1000), 						-- �̹��� �̸�
-	fileExtension varchar2(10),					-- �̹��� Ȯ����
-	fileFullNm varchar2(1000),					-- �̹��� �̸�+Ȯ����
-	fileRoute varchar2(1000)					-- �̹��� ���
+	fileNm varchar2(1000), 						-- 이미지 이름
+	fileExtension varchar2(10),					-- 이미지 확장자
+	fileFullNm varchar2(1000),					-- 이미지 이름+확장자
+	fileRoute varchar2(1000)					-- 이미지 경로
 )
 
 
 
 SELECT * FROM ADMIN;
-SELECT * FROM "MEMBER";
+SELECT * FROM MEMBER;
 SELECT * FROM GOODS;
 SELECT * FROM CART;
 SELECT * FROM ORDERS;
